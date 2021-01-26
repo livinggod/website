cfa5f9f7239ff387facae68fdd2f0ac1d3a54131
Quinten <quinten@justbetter.nl>
2021-01-26 22:35:21 +0100

    Can use link as image / remove avatar

--- resources/views/components/post-card.blade.php
+++ resources/views/components/post-card.blade.php
@@ -1,8 +1,8 @@
-<div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+<div {{ $attributes->merge(['class' => 'flex flex-col rounded-lg shadow-lg overflow-hidden']) }}>
     <div class="flex-shrink-0">
         <a href="{{ route('post.show', $post) }}">
             <img class="h-48 w-full object-cover"
-                 src="{{ asset('storage/' . $post->image) }}"
+                 src="{{ \Illuminate\Support\Str::contains($post->image, '://') ? $post->image : asset('storage/' . $post->image) }}"
                  alt="">
         </a>
     </div>
@@ -22,16 +22,8 @@
                 </p>
             </a>
         </div>
-        <div class="mt-6 flex items-center">
-            <div class="flex-shrink-0">
-                <a href="#">
-                    <span class="sr-only">Quinten Buis</span>
-                    <img class="h-10 w-10 rounded-full"
-                         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                         alt="">
-                </a>
-            </div>
-            <div class="ml-3">
+        <div class="mt-6">
+            <div>
                 <p class="text-sm font-medium text-gray-900">
                     <a href="#" class="hover:underline">
                         Quinten Buis
