18cffb2c5a30917c988133f6e306034924e64a0f
Quinten <quinten@justbetter.nl>
2021-01-29 22:47:55 +0100

    Change footer columns and links to component

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -6,93 +6,13 @@
                 <div class="md:grid md:grid-cols-2 md:gap-8">
                     <x-footer.column title="test">
                         <x-footer.link class="" href="#">Testing</x-footer.link>
-
-                        <li>
-                            <a href="#" class="text-base text-gray-500 hover:text-gray-900">
-                                Analytics
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href="#" class="text-base text-gray-500 hover:text-gray-900">
-                                Commerce
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href="#" class="text-base text-gray-500 hover:text-gray-900">
-                                Insights
-                            </a>
-                        </li>
                     </x-footer.column>
-                    <div class="mt-12 md:mt-0">
-                        <h3 class="text-sm font-semibold text-gray-400 tracking-wider uppercase">
-                            Support
-                        </h3>
-                        <ul class="mt-4 space-y-4">
-                            <li>
-                                <a href="#" class="text-base text-gray-500 hover:text-gray-900">
-                                    Pricing
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="#" class="text-base text-gray-500 hover:text-gray-900">
-                                    Documentation
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="#" class="text-base text-gray-500 hover:text-gray-900">
-                                    Guides
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="#" class="text-base text-gray-500 hover:text-gray-900">
-                                    API Status
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="md:grid md:grid-cols-2 md:gap-8">
-                    <div>
-                        <h3 class="text-sm font-semibold text-gray-400 tracking-wider uppercase">
-                            Company
-                        </h3>
-                        <ul class="mt-4 space-y-4">
-                            <li>
-                                <a href="#" class="text-base text-gray-500 hover:text-gray-900">
-                                    About
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="#" class="text-base text-gray-500 hover:text-gray-900">
-                                    Blog
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="#" class="text-base text-gray-500 hover:text-gray-900">
-                                    Jobs
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="#" class="text-base text-gray-500 hover:text-gray-900">
-                                    Press
-                                </a>
-                            </li>
-
-                            <li>
-                                <a href="#" class="text-base text-gray-500 hover:text-gray-900">
-                                    Partners
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
+                    <x-footer.column title="test">
+                        <x-footer.link class="" href="#">Testing</x-footer.link>
+                    </x-footer.column>
+                    <x-footer.column title="test">
+                        <x-footer.link class="" href="#">Testing</x-footer.link>
+                    </x-footer.column>
                     <div class="mt-12 md:mt-0">
                         <h3 class="text-sm font-semibold text-gray-400 tracking-wider uppercase">
                             Legal
@@ -125,9 +45,12 @@
                 </h3>
                 <form class="mt-4 sm:flex sm:max-w-md">
                     <label for="emailAddress" class="sr-only">Email address</label>
-                    <input type="email" name="emailAddress" id="emailAddress" autocomplete="email" required class="appearance-none min-w-0 w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-4 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:placeholder-gray-400" placeholder="Enter your email">
+                    <input type="email" name="emailAddress" id="emailAddress" autocomplete="email" required
+                           class="appearance-none min-w-0 w-full bg-white border border-gray-300 rounded-md shadow-sm py-2 px-4 text-base text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:placeholder-gray-400"
+                           placeholder="Enter your email">
                     <div class="mt-3 rounded-md sm:mt-0 sm:ml-3 sm:flex-shrink-0">
-                        <button type="submit" class="w-full bg-custom-green-100 flex items-center justify-center border border-transparent rounded-md py-2 px-4 text-base font-medium text-white focus:outline-none hover:bg-custom-green-200 focus:bg-custom-green-300">
+                        <button type="submit"
+                                class="w-full bg-custom-green-100 flex items-center justify-center border border-transparent rounded-md py-2 px-4 text-base font-medium text-white focus:outline-none hover:bg-custom-green-200 focus:bg-custom-green-300">
                             Subscribe
                         </button>
                     </div>
