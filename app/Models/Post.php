a3e1cbbb0a1d0ea03c1a05ea21f69a128b257c71
Quinten <quinten@justbetter.nl>
2021-01-31 10:59:07 +0100

    Fix cast issue
    Signed-off-by: Quinten <quinten@justbetter.nl>

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,6 +11,10 @@ class Post extends Model
 
     protected $guarded = [];
 
+    protected $casts = [
+        'publish_at' => 'datetime'
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
