898380b6ad662b6a392a3ec790243c28c3f89aef
Quinten <quinten@justbetter.nl>
2021-02-11 21:50:33 +0100

    Cookie banner responsive

--- resources/views/vendor/cookieConsent/dialogContents.blade.php
+++ resources/views/vendor/cookieConsent/dialogContents.blade.php
@@ -1,14 +1,14 @@
 <div class="fixed transition duration-200 opacity-0 bottom-0 inset-x-0 pb-2 sm:pb-5 js-cookie-consent cookie-consent tran">
     <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div class="p-2 rounded-lg bg-custom-green-100 shadow-lg sm:p-3">
-            <div class="flex items-center justify-between flex-wrap">
-                <div class="w-0 flex-1 flex items-center">
-                    <p class="ml-3 font-medium text-white truncate">
+            <div class="md:flex items-center justify-between flex-wrap">
+                <div class="md:w-0 flex-1 flex items-center">
+                    <p class="ml-3 font-medium text-white">
                         {!! trans('cookieConsent::texts.message') !!}
                     </p>
                 </div>
-                <div class="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
-                    <button class="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-custom-green-100 bg-white hover:bg-custom-green-200 focus:outline-none js-cookie-consent-agree cookie-consent__agree">
+                <div class="order-3 mt-8 flex-shrink-0 w-full sm:order-2 sm:mt-0 md:w-auto">
+                    <button class="flex mx-auto items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-custom-green-100 bg-white hover:bg-custom-green-200 focus:outline-none js-cookie-consent-agree cookie-consent__agree">
                         {{ trans('cookieConsent::texts.agree') }}
                     </button>
                 </div>
