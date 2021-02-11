cea3df5f5c8c99e52da96f2f598490205a09bd6e
Quinten <quinten@justbetter.nl>
2021-02-11 21:29:29 +0100

    Fix Tests
    Signed-off-by: Quinten <quinten@justbetter.nl>

--- tests/Feature/pages/HomeTest.php
+++ tests/Feature/pages/HomeTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\pages;
 
 use App\Models\Post;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
 class HomeTest extends TestCase
@@ -12,6 +13,7 @@ class HomeTest extends TestCase
 
     public function test_homepage_status_loads()
     {
+        Storage::fake('public');
         Post::factory()->create();
 
         $response = $this->get('/');
