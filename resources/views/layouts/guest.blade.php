d0d0ae30792e1f7b93b1066f7fd61e86d512ebf2
Quinten <quinten@justbetter.nl>
2021-02-27 18:11:32 +0100

    Add hotjar

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -39,6 +39,19 @@
     <meta name="og:description" content="{{ $description ?? '' }}">
 
     <livewire:styles />
+
+    <!-- Hotjar Tracking Code for www.livinggod.net -->
+    <script>
+        (function(h,o,t,j,a,r){
+            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+            h._hjSettings={hjid:2270818,hjsv:6};
+            a=o.getElementsByTagName('head')[0];
+            r=o.createElement('script');r.async=1;
+            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+    </script>
+
 </head>
 <body x-data="{mobilemenu: false}" class="h-full" :class="{ 'overflow-y-hidden': mobilemenu === true}">
 <x-navbar/>
