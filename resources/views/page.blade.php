1f7513852f002ba3cbbd19dd660cd846c4bc4a21
Quinten <quinten@justbetter.nl>
2021-04-10 21:28:06 +0200

    Set Tailwind to JIT

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -4,7 +4,7 @@
             <div class="mt-6 prose prose-lg text-gray-500 mx-auto">
                 <article class="text-lg max-w-prose leading-9 mx-auto">
 
-                    <div class="relative">
+                    <div class="relative min-h-[400px]">
                         <img class="mx-auto rounded-lg" src="{{ asset('storage/resizes/700x700/' . $page->image) }}" alt="">
                         <div class="absolute inset-0 bg-gradient-to-t from-black rounded-lg"></div>
                         <p class="absolute bottom-0 left-0 text-white font-bold text-2xl md:text-4xl mx-6">{{ $page->title }}</p>
