74a8dba4190adb50939113dbd1cea1ba1a2ae7bf
Quinten Buis <quinten@justbetter.nl>
2021-06-19 16:19:06 +0200

    Allow localization on uri

--- app/Http/Controllers/RedirectController.php
+++ app/Http/Controllers/RedirectController.php
@@ -4,13 +4,29 @@ namespace App\Http\Controllers;
 
 use App\Http\Response\ResponseFactory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Quintenbuis\Localization\Locale;
 
 class RedirectController extends Controller
 {
-    public function __invoke(Request $request, ?string $slug = '/'): View
+    public function __invoke(Request $request, string $lang = '/', ?string $slug = '/'): View|RedirectResponse
     {
         session()->remove('active');
+
+        $locale = new Locale();
+
+        if ($locale->isDefault($lang)) {
+            return redirect(rtrim(config('app.url'), '/') . '/' . $slug);
+        }
+
+        if (! $locale->isCurrent($lang) && $locale->isSupported($lang)) {
+            $locale->set($lang);
+        }
+
+        if (! $locale->isCurrent($lang) && ! $locale->isSupported($lang)) {
+            $slug = $lang;
+        }
 
         $response = ResponseFactory::createFromSlug($slug);
 
