885e18879dba49f7ca26647eabc2b2f4e0eacc48
Quinten Buis <quinten@justbetter.nl>
2021-09-08 13:39:48 +0200

    Set version in base url

--- app/Domain/SendPortal/Integrations/SendPortal.php
+++ app/Domain/SendPortal/Integrations/SendPortal.php
@@ -20,7 +20,7 @@ class SendPortal
 
     protected function getBaseUrl(): string
     {
-        return $this->fallbackSlash(config('sendportal.url')).$this->fallbackSlash(config('sendportal.version'));
+        return $this->fallbackSlash(config('sendportal.url'));
     }
 
     protected function fallbackSlash(string $item): string
--- config/sendportal.php
+++ config/sendportal.php
@@ -3,7 +3,5 @@
 return [
     'url' => env('SENDPORTAL_BASE_URL'),
 
-    'version' => env('SENDPORTAL_VERSION', 'v1'),
-
     'api_key' => env('SENDPORTAL_API_KEY'),
 ];
