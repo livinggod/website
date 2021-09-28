1464a25ab059df89a3fa92d76a4f372f66ea7832
Quinten Buis <quinten@justbetter.nl>
2021-09-28 21:39:25 +0200

    Fix error on opt in completion

--- tests/Feature/NewsletterTest.php
+++ tests/Feature/NewsletterTest.php
@@ -3,9 +3,12 @@
 namespace Tests\Feature;
 
 use App\Domain\SendPortal\Facades\SendPortal;
+use App\Domain\SendPortal\Models\Subscriber;
+use App\Extensions\Locale\Locale;
 use App\Mail\NewsletterOptInMail;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\Str;
 use Tests\TestCase;
 
@@ -41,7 +44,24 @@ class NewsletterTest extends TestCase
         Mail::assertNotQueued(NewsletterOptInMail::class);
     }
 
-    protected function fakeSubscriber()
+    /** @test */
+    public function it_redirects_to_correct_language_after_opting_in(): void
+    {
+        SendPortal::shouldReceive('get')->andReturn(['data' => []]);
+        SendPortal::shouldReceive('post')->andReturn(['data'=> ['tags' => []]]);
+
+        $url = URL::temporarySignedRoute(
+            'opt-in', now()->addMinutes(120), ['email' => 'test@test.com', 'language' => 'nl']
+        );
+
+        $this
+            ->get($url)
+            ->assertRedirect(
+                uri: Locale::redirectToLocale('nl', '/')
+            );
+    }
+
+    protected function fakeSubscriber(): array
     {
         return [
             'data' => [
