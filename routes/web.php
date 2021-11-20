ef542e13d348a1194418ad86771d7da8782f2310
Quinten Buis <quinten@justbetter.nl>
2021-11-20 20:57:01 +0100

    Extract image resizer to module

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,6 @@ use App\Http\Controllers\RedirectController;
 use App\Http\Controllers\RedirectSubdomainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/storage/resizes/{size}/{file}', ImageController::class)->where('file', '.*');
-
 Route::domain('{locale}.livinggod.net')->group(function () {
     Route::get('/{url}', RedirectSubdomainController::class)->where(['url' => '.*', 'locale' => '(\b[a-zA-Z]{2}\b)']);
 });
@@ -17,5 +15,5 @@ Route::post('/newsletter', [NewsletterController::class, 'store'])->name('newsle
 Route::get('/opt-in/{email}/{language?}', NewsletterOptInController::class)->name('opt-in')->middleware('signed');
 
 Route::get('/{slug?}', RedirectController::class)
-    ->where('slug', '^(?!nova).*$')
+    ->where('slug', '^(?!nova|storage).*$')
     ->name('page');
