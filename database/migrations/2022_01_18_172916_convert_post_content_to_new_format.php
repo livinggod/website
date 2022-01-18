4fc4c97fd2fd8fc4d67be6a159aac8656235d264
Quinten Buis <quinten.buis@gmail.com>
2022-01-18 22:00:58 +0100

    Migrate old content data to new editor

---
+++ database/migrations/2022_01_18_172916_convert_post_content_to_new_format.php
@@ -0,0 +1,92 @@
+<?php
+
+use App\Models\Post;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Post::query()->cursor()->each(function (Post $post) {
+            foreach (array_unique(config('filament-spatie-laravel-translatable-plugin.default_locales')) as $locale) {
+                $newContent = [];
+                $content = $post->getTranslation('content', $locale);
+
+                if (is_array($content)) { // Newer format
+                    if (empty($content) || isset(reset($content)['type'])) {
+                        continue; // already converted
+                    }
+
+                    foreach ($content as $block) {
+                        if (isset($block['type']) || $block['layout'] ?? '' !== 'wysiwyg') {
+                            continue; // already converted
+                        }
+
+                        if (isset($block['attributes']['title'])) {
+                            $newContent[] = [
+                                'data' => [
+                                    'title' => $block['attributes']['title'],
+                                ],
+                                'type' => 'title',
+                            ];
+                        }
+
+                        if (isset($block['attributes']['content'])) {
+                            $newContent[] = [
+                                'data' => [
+                                    'content' => $block['attributes']['content'],
+                                ],
+                                'type' => 'paragraph',
+                            ];
+                        }
+                    }
+                } else {
+
+                    $raw = json_decode($content, true);
+
+                    if (! isset($raw['blocks'])) {
+                        continue; // No content
+                    }
+
+                    $blocks = $raw['blocks'];
+
+                    foreach ($blocks as $block) {
+
+                        if (isset($block['data']['style'])) { // Bullet lists
+                            foreach ($block['data']['items'] as $item) {
+                                $newContent[] = [
+                                    'data' => [
+                                        'content' => '* '.$item,
+                                    ],
+                                    'type' => 'paragraph',
+                                ];
+                            }
+
+                            continue;
+                        }
+
+                        if (isset($block['data']['file'])) { // files
+                            $newContent[] = [
+                                'data' => [
+                                    'content' => "![image]({$block['data']['file']['url']})",
+                                ],
+                                'type' => 'paragraph',
+                            ];
+
+                            continue;
+                        }
+
+                        $newContent[] = [
+                            'data' => [
+                                'content' => $block['data']['text'],
+                            ],
+                            'type' => 'paragraph',
+                        ];
+                    }
+                }
+                $post->setTranslation('content', $locale, $newContent);
+            }
+
+            $post->save();
+        });
+    }
+};
