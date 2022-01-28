accc05e3b3fe00d0e3903c60ee185b70585d1fe5
Quinten Buis <quinten.buis@gmail.com>
2022-01-28 21:06:30 +0100

    Convert images to spatie medialibrary

---
+++ database/migrations/2022_01_28_194413_migrate_images_to_spatie_medialibrary.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up()
+    {
+        $queries = [
+            [\App\Models\Page::query(), 'image'],
+            [\App\Models\Post::query(), 'image'],
+            [\App\Models\User::query(), 'avatar']
+        ];
+
+        foreach ($queries as [$query, $imageField]) {
+            $query->cursor()->each(function (\Illuminate\Database\Eloquent\Model $model) use ($imageField) {
+                if (\Illuminate\Support\Facades\Storage::disk('public')->exists($model->$imageField)) {
+                    $model->addMedia(
+                        \Illuminate\Support\Facades\Storage::disk('public')->path($model->$imageField)
+                    )->toMediaCollection('default');
+                }
+
+                $model->save();
+            });
+        }
+    }
+};
