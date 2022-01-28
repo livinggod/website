38b9b7716b8d3580e4ac52049b1f790a0844b776
Quinten Buis <quinten.buis@gmail.com>
2022-01-28 21:30:42 +0100

    Set post image from medialibrary

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -23,7 +23,7 @@
                             class="mt-2 block text-center leading-8 tracking-tight text-gray-400 text-base sm:text-lg">{{ $post->description }}</span>
                     </h1>
 
-                    <img class="mx-auto rounded-lg" src="{{ asset('storage/' . $post->image) }}" alt="">
+                    <img class="mx-auto rounded-lg" src="{{ $post->getFirstMedia()->getUrl() }}" alt="">
 
                     <div class="flex m-0">
                         <img class="w-12 h-12 md:w-14 md:h-14 object-cover rounded-full"
@@ -36,11 +36,7 @@
                     </div>
 
                     <div id="article-content">
-                        @if(is_array($post->content))
-                            @flexible($post->content)
-                        @else
-                            {!! \Advoor\NovaEditorJs\NovaEditorJs::generateHtmlOutput($post->content) !!}
-                        @endif
+                        @flexible($post->content)
                     </div>
                 </article>
             </div>
