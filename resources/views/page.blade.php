f509da555e1dc9054f5b3525509fea7424e78719
Quinten Buis <quinten.buis@gmail.com>
2022-02-11 21:33:54 +0100

    Fix missing image on pages

--- resources/views/page.blade.php
+++ resources/views/page.blade.php
@@ -8,7 +8,7 @@
                 <article class="text-lg max-w-prose leading-9 mx-auto">
 
                     <div class="relative min-h-[400px]">
-                        <img class="mx-auto rounded-lg" src="{{ $page->getFirstMediaUrl('image') }}" alt="">
+                        <img class="mx-auto rounded-lg" src="{{ $page->getFirstMediaUrl() }}" alt="">
                         <div class="absolute inset-0 bg-gradient-to-t from-black rounded-lg"></div>
                         <p class="absolute bottom-0 left-0 text-white font-bold text-2xl md:text-4xl mx-6">{{ $page->title }}</p>
                     </div>
